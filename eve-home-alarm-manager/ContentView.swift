a717e4dde25f00f10171680073734824ffd27bb6
Kevin <weiyih@sheridancollege.ca>
2019-12-11 12:02:43 -0500

    Initial Commit

---
+++ eve-home-alarm-manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eve-home-alarm-manager
+//
+//  Created by Kevin Wei on 2019-12-11.
+//  Copyright © 2019 Kevin Wei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
